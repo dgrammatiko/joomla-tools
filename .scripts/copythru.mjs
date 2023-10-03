afe4f26d354eb1c9c828b0c5ac07fff9de8a4340
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-10-03 15:21:06 +0300

    Deal with the Joomla.asset.json files (#139)
    * Update copythru.mjs
    
    * Update joomla-tools.mjs
    
    * skipt the templates as the files are not in the media folder

--- .scripts/copythru.mjs
+++ .scripts/copythru.mjs
@@ -44,6 +44,18 @@ async function copyThru(path) {
   jetpack
     .find(globalThis.searchPath, { matching: 'images', files: false, directories: true })
     .forEach((file) => jetpack.copy(file, file.replace(`${globalThis.searchPath}${sep}`, `${globalThis.replacePath}${sep}`), { overwrite: true }));
+
+  // Copy the joomla.asset.json files
+  if (existsSync('./media')) {
+    readdirSync('./media').forEach((ext) => {
+      if (ext !== 'templates') {
+        if (existsSync(`${process.cwd()}/media_source/${ext}/joomla.asset.json`) && !existsSync(`${process.cwd()}/media/${ext}/joomla.asset.json`)) {
+          if (!existsSync(`${process.cwd()}/media/${ext}`)) mkdirSync(`${process.cwd()}/media/${ext}`, {recursive: true});
+          if (!existsSync(`${process.cwd()}/media/${ext}/joomla.asset.json`)) copyFileSync(`${process.cwd()}/media_source/${ext}/joomla.asset.json`, `${process.cwd()}/media/${ext}/joomla.asset.json`);
+        }
+      }
+    });
+  }
 };
 
 export { copyThru };
