50a4d52cf2e8d7db246222cf211db961a118cba5
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-11-28 01:59:49 +0200

    eslint => biome, npm => pnpm

--- joomla-tools.mjs
+++ joomla-tools.mjs
@@ -93,4 +93,21 @@ async function main() {
   }
 }
 
+console.log(`%c
+   d88b  .d88b.   .d88b.  .88b  d88. db       .d8b.
+    8P  .8P  Y8. .8P  Y8. 88 YbdP 88 88      d8   8b
+    88  88    88 88    88 88  88  88 88      88   88
+    88  88    88 88    88 88  88  88 88      8888888
+db. 88   8b  d8   8b  d8  88  88  88 88      88   88
+Y8888P    Y88P     Y88P   YP  YP  YP Y88888P YP   YP
+
+
+d888888b  .d88b.   .d88b.  db      .d8888.
+   88    .8P  Y8. .8P  Y8. 88      88
+   88    88    88 88    88 88       8bo.
+   88    88    88 88    88 88         Y8b.
+   88     8b  d8   8b  d8  88booo.      8D
+   YP      Y88P     Y88P   Y88888P  8888Y `,
+  'font-family:monospace; color: orange;',
+);
 main();
