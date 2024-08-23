718efac9c6910a86abae7beb5a71073d5b6e8a98
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2024-08-23 20:20:16 +0300

    move test stubs

--- test/css.test.js
+++ test/css.test.js
@@ -6,12 +6,9 @@ import { handleCssFile } from '../.scripts/stylesheets/handleCSSFile.mjs';
 
 describe('CSS handling tests', { concurrency: false }, () => {
   // Cleanup
-  test.afterEach(() => {
-    if (existsSync('test/stubs/new/css')) rmSync('test/stubs/new/css', { force: true, recursive: true });
+  test.afterEach(async () => {
+    if (existsSync('test/stubs/css')) rmSync('media/stubs/css', { force: true, recursive: true });
   });
-  // test.beforeEach(() => {
-  //   if (existsSync('test/stubs/new/css')) rmSync('test/stubs/new/css', { force: true, recursive: true });
-  // });
 
   it('Non existing CSS file', (t) => {
     process.env.production = 'production';
@@ -27,23 +24,22 @@ describe('CSS handling tests', { concurrency: false }, () => {
     assert.equal(message, `File ${inputFile} doesn't exist`);
   });
 
-  // This needs infra
-  // it('CSS file no outputFile argument [production]', (t) => {
-  //   process.env.production = 'production';
-  //   const file = 'css_without_import.css';
-  //   const inputFile = `test/stubs/css/${file}`;
-  //   const outputFile = `test/stubs/new/css/${file.replace('.css', '.min.css')}`;
+  it('CSS file no outputFile argument [production]', (t) => {
+    process.env.production = 'production';
+    const file = 'css_without_import.css';
+    const inputFile = `media_source/stubs/css/${file}`;
+    const outputFile = `media/stubs/css/${file.replace('.css', '.min.css')}`;
 
-  //   handleCssFile(inputFile);
-  //   assert.equal(existsSync(outputFile), true);
-  //   assert.equal(readFileSync(outputFile, { encoding: 'utf8' }), 'body{color:red}\n/*# sourceMappingURL=css_without_import.min.css.map */');
-  // });
+    handleCssFile(inputFile);
+    assert.equal(existsSync(outputFile), true);
+    assert.equal(readFileSync(outputFile, { encoding: 'utf8' }), 'body{color:red}\n/*# sourceMappingURL=css_without_import.min.css.map */');
+  });
 
   it('CSS file without import [production]', (t) => {
     process.env.production = 'production';
     const file = 'css_without_import.css';
-    const inputFile = `test/stubs/css/${file}`;
-    const outputFile = `test/stubs/new/css/${file.replace('.css', '.min.css')}`;
+    const inputFile = `media_source/stubs/css/${file}`;
+    const outputFile = `media/stubs/css/${file.replace('.css', '.min.css')}`;
 
     handleCssFile(inputFile, outputFile);
     assert.equal(existsSync(outputFile), true);
@@ -56,22 +52,22 @@ describe('CSS handling tests', { concurrency: false }, () => {
   it('CSS file without import [development]', (t) => {
     process.env.production = 'nope';
     const file = 'css_without_import.css';
-    const inputFile = `test/stubs/css/${file}`;
-    const outputFile = `test/stubs/new/css/${file.replace('.css', '.min.css')}`;
+    const inputFile = `media_source/stubs/css/${file}`;
+    const outputFile = `media/stubs/css/${file.replace('.css', '.min.css')}`;
 
     handleCssFile(inputFile, outputFile);
     assert.equal(existsSync(outputFile), true);
     assert.equal(
       readFileSync(outputFile, { encoding: 'utf8' }),
-      'body {\n  color: red;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VSb290IjpudWxsLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbInRlc3Qvc3R1YnMvY3NzL2Nzc193aXRob3V0X2ltcG9ydC5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiYm9keSB7XG4gIGNvbG9yOiByZWQ7XG59XG4iXSwibmFtZXMiOltdfQ== */',
+      'body {\n  color: red;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VSb290IjpudWxsLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIm1lZGlhX3NvdXJjZS9zdHVicy9jc3MvY3NzX3dpdGhvdXRfaW1wb3J0LmNzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJib2R5IHtcbiAgY29sb3I6IHJlZDtcbn1cbiJdLCJuYW1lcyI6W119 */',
     );
   });
 
   it('CSS file with import', (t) => {
     process.env.production = 'production';
     const file = 'css_with_import.css';
-    const inputFile = `test/stubs/css/${file}`;
-    const outputFile = `test/stubs/new/css/${file.replace('.css', '.min.css')}`;
+    const inputFile = `media_source/stubs/css/${file}`;
+    const outputFile = `media/stubs/css/${file.replace('.css', '.min.css')}`;
 
     handleCssFile(inputFile, outputFile);
     assert.equal(existsSync(outputFile), true);
